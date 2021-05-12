dcd22fa25ee48deecd136891e5e7c462bcbe7c49
Lindsey JJ <ljjlauria@mac.com>
2021-05-12 16:35:20 -0700

    Got license link section to generate properly. README output looks to be working

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const md = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TCreate an array of questions for user input
+const questions = [
+    {
+        message: 'What is the title of your project?',
+        name: 'title',
+        default: 'Project Title',
+    },
+    {
+        message: 'What problem does this application solve?',
+        name: 'solves',
+    },
+    {
+        message: 'Why did you build this project?',
+        name: 'why',
+    },
+    {
+        message: 'What technologies are used?',
+        name: 'technologies',
+    },
+    {
+        message: 'What are the steps required to install your project?',
+        name: 'instructions',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        message: 'Provide instructions and examples for use.',
+        name: 'usage',
+    },
+     {
+        message: 'Choose a license:',
+        name: 'license',
+        type: 'list',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+    },
+    {
+        message: 'What are the guidelines for contributing to the project?',
+        name: 'guidelines',
+    },
+    {
+        message: 'Provide instructions on how to run tests for the application.',
+        name: 'tests',
+    },
+    {
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        message: 'What is your email address?',
+        name: 'email',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+];
+
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, (err) =>
+      err ? console.log('An error occurred while creating your README file.') : console.log('Your README file was successfully created.')
+    );
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+      .then((data) => {
+          console.log('data for readme', data);
+        const readme = md.generateMarkdown(data);
+        writeToFile('README', readme);
+    });
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,137 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in.
+function renderLicenseBadge(license) {
+    // `Switch` statement that accepts an `license` parameter and assigns the appropriate URL to the badge to display. 
+    // If there is no license, return an empty string.
+    let badge = ' ';
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    switch(license) {
+      case 'GNU AGPLv3':
+        badge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+        break;
+      case 'GNU GPLv3':
+        badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        break;
+      case 'GNU LGPLv3':
+        badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+        break;
+      case 'Mozilla Public License 2.0':
+        badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        break;
+      case 'Apache License 2.0':
+        badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        break;
+      case 'MIT License':
+        badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        break;
+      case 'Boost Software License 1.0':
+        badge = '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+        break;
+      case 'The Unlicense':
+        badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        break;
+      default:
+        break;
+    }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    return badge;
+    
+}
 
-// TODO: Create a function to generate markdown for README
+// Function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+      // `Switch` statement that accepts an `license` parameter and assigns the appropriate URL to display. 
+    let licenseWeb = ' ';
+    
+    switch(license) {
+      case 'GNU AGPLv3':
+        licenseWeb = 'https://spdx.org/licenses/AGPL-3.0-or-later.html';
+        break;
+      case 'GNU GPLv3':
+        licenseWeb = 'https://spdx.org/licenses/GPL-3.0-or-later.html';
+        break;
+      case 'GNU LGPLv3':
+        licenseWeb = 'https://spdx.org/licenses/LGPL-3.0-or-later.html';
+        break;
+      case 'Mozilla Public License 2.0':
+        licenseWeb = 'https://spdx.org/licenses/MPL-2.0.html';
+        break;
+      case 'Apache License 2.0':
+        licenseWeb = 'https://www.apache.org/licenses/LICENSE-2.0.html';
+        break;
+      case 'MIT License':
+        licenseWeb = 'https://spdx.org/licenses/MIT.html';
+        break;
+      case 'Boost Software License 1.0':
+        licenseWeb = 'https://spdx.org/licenses/BSL-1.0.html';
+        break;
+      case 'The Unlicense':
+        licenseWeb = 'https://unlicense.org';
+        break;
+      default:
+        break;
+    }
+    
+    return licenseWeb;
+
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    const licenseURL = renderLicenseLink(license);
+    console.log('licenseURL', licenseURL);
+    const licenseSectionText = `Licensed under [${license}](${licenseURL}).`;
+    return licenseSectionText;
+}
+
+// Function to generate markdown for README
+// TODO: update video link
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const license = data.license;
+  const licenseSection = renderLicenseSection(license);
+  const licenseBadge = renderLicenseBadge(license);
 
+  return `# ${data.title}
+  ${licenseBadge}
+
+  ## Description
+  ${data.solves} ${data.why} ${data.technologies}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.instructions}
+  [Install Node.js and npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+  ## Usage
+  ${data.usage}
+  :movie_camera: [Walkthrough video](http://???)
+  :camera: ![Readme screenshot](assets/images/screenshot.png)
+
+  ## License
+  ${licenseSection}
+
+  ## Contributing
+  ${data.guidelines}
+  If you are interested in fixing issues and contributing to this project, please refer to the [Contributor Covenant](https://www.contributor-covenant.org/).
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  For more information about me and my other projects, visit my [GitHub profile](https://github.com/${data.github}).
+  Reach me to report issues or with questions at [${data.email}](mailto:${data.email}).
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown
+};
